adc35a06ebbe0aa0e9073b01110751bb56a9d572
Esse Dansomon <essedansomon@gmail.com>
2023-03-03 22:42:48 +0000

    add login and logout button

--- src/main/resources/frontend/src/app/app.component.ts
+++ src/main/resources/frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {OAuthService} from "angular-oauth2-oidc";
+import {authconfig} from "../../auth.config";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  constructor(private oauthService: OAuthService, private appService : AppService) {
+    this.configure();
+    this.appService.getHello().subscribe((data) => {
+      console.log(data);
+    });
+
+  }
+
+  private configure() {
+    this.oauthService.configure(authconfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
 }
--- src/main/resources/frontend/src/app/app.module.ts
+++ src/main/resources/frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {OAuthModule} from "angular-oauth2-oidc";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot(
+      {
+        resourceServer: {
+          allowedUrls: ['http://localhost:8101/api'],
+          sendAccessToken: true
+        }
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
